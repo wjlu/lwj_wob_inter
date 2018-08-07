19a61b2370f16fb9003b4a3d5a1750a482c3b9a3
amen <llwj.198@163.com>
2018-08-07 09:44:10 +0800

    8.7a

--- testcase/start.py
+++ testcase/start.py
@@ -22,21 +22,13 @@ def allTestCase():
     return suite
 
 
-def run():
-
-    HTMLTestRunner.HTMLTestRunner(
-        stream=fp,
-        title='wob商城接口自动化报告',
-        description='wob商城接口自动化报告，用于定时检测接口正确性'
-    ).run(allTestCase())
-
 if __name__ =="__main__":
     timestr=time.strftime('%Y%m%d%H',time.localtime(time.time()))
-    filename = timestr+'AutoTest.html'
+
     with open('report.html', 'wb') as f:
         runner=HTMLTestRunner.HTMLTestRunner(
             stream=f,
-            title='wob商城接口自动化报告',
+            title=timestr+'wob商城接口自动化报告',
             description='wob商城接口自动化报告，用于定时检测接口正确性'
         ).run(allTestCase())
 
